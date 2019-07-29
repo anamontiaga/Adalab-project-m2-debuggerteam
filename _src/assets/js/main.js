439e4c8fbd353fba9516850d1ca385d7a10b65c4
Mar Prieto <mar.pnieto@gmail.com>
2019-07-29 10:31:37 +0200

    Fixed js files

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,24 +1,3 @@
-"use strict";
-
-// Acción boton
-// const startButton = document.querySelector(".layout-page__button");
-
-// function handlerGoHome() {
-//   const getHome = (window.location.href = "home.html#home");
-//   return getHome;
-// }
-
-// startButton.addEventListener("click", handlerGoHome);
-
-// // Focus onclick
-// const getBody = document.querySelector(".layout");
-
-// function getFocus() {
-//   startButton.focus();
-// }
-
-// getBody.addEventListener("click", getFocus);
-
 // collapse ---------------------------------------------
 
 // select collpse button
