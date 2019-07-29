8296e841766a47191626496495502b0d1e6f7f02
Mar Prieto <mar.pnieto@gmail.com>
2019-07-29 10:44:13 +0200

    change js in gulpfile and config.js

--- _src/assets/js/landing.js
+++ _src/assets/js/landing.js
@@ -1,22 +1,18 @@
 "use strict";
 
-console.log("landing");
-
 //Acción boton
-const startButton = document.querySelector(".layout-page__button");
-
 function handlerGoHome() {
   const getHome = (window.location.href = "home.html#home");
   return getHome;
 }
+function getFocus() {
+  homeBtn.focus();
+}
+const homeBtn = document.querySelector(".layout-page__button");
 
-startButton.addEventListener("click", handlerGoHome);
-
+homeBtn.addEventListener("click", handlerGoHome);
+debugger;
 // Focus onclick
 const getBody = document.querySelector(".layout");
-
-function getFocus() {
-  startButton.focus();
-}
 
 getBody.addEventListener("click", getFocus);
